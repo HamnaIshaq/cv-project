5c6d32d13d02be0f7f116a38ab2f6fe6b4b567b2
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-12 22:06:54 +0500

    Show education in final CV

--- src/components/FinalCV.js
+++ src/components/FinalCV.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 
 class FinalCV extends Component {
   render() {
-    const { generalInfo } = this.props.cvInfo;
+    const { generalInfo, education } = this.props.cvInfo;
 
     const { name, email, phoneNumber } = generalInfo;
+    //const { schoolName, degreeName, startDate, endDate } =
+
+    const educationList = education.map((ed) => {
+      return (
+        <div key={ed.id}>
+          <p>School: {ed.schoolName}</p>
+          <p>Degree Name: {ed.degreeName}</p>
+          <p>
+            {ed.startDate} - {ed.present ? "Present" : ed.endDate}
+          </p>
+        </div>
+      );
+    });
 
     return (
       <div>
         <h1>{name}</h1>
         <p>{email}</p>
         <p>{phoneNumber}</p>
+
+        <h2>Education</h2>
+        <div>{educationList}</div>
       </div>
     );
   }
